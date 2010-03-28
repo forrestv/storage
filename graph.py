0e510f7f6955a74b6ec753fb0cdaea071394cad7
(no author) <(no author)@470744a7-cac9-478e-843e-5ec1b25c69e8>
2010-03-28 05:14:56 +0000

    assimilation of flash
    git-svn-id: svn://forre.st/storage@943 470744a7-cac9-478e-843e-5ec1b25c69e8

--- graph.py
+++ graph.py
@@ -4,7 +4,7 @@ import time
 
 import Gnuplot
 
-def graph(myitems, title, out_name):
+def graph(myitems, out_name):
   tmp_name = out_name+".tmp"+str(random.randrange(2**24))
   g = Gnuplot.Gnuplot(debug=1)
   g("set terminal png size 600,400 nocrop x000000 xFFFFFF xFFFFFF xFF0000 x00FF00 x0000FF")
@@ -12,22 +12,16 @@ def graph(myitems, title, out_name):
   g("set xtics nomirror")
   g("set ytics nomirror")
   g.set_string("output", tmp_name)
-  if title:
-      g.title(title)
   g.xlabel('Size in GB')
   g.ylabel('GB/$');
   g("set style line 1 lc 1")
   g("set style line 2 lc 2")
   g("set style line 3 lc 3")
+  g("set style line 4 lc 4")
   l = []
-  ll = [
-      ([(x[0],x[0]/x[1]) for x in myitems if x[2][0:4] != "Open" and x[2][-3:] != "OEM"], 'Retail'),
-      ([(x[0],x[0]/x[1]) for x in myitems if x[2][0:4] != "Open" and x[2][-3:] == "OEM"], 'OEM'),
-      ([(x[0],x[0]/x[1]) for x in myitems if x[2][0:4] == "Open"], 'Open'),
-  ]
-  for i, (d,title) in enumerate(ll):
-    if d:
-      l.append(Gnuplot.Data(d, **{'with':"points ls %i"%(i+1), 'title':title}))
+  for i, (k, v) in enumerate(myitems.iteritems()):
+    if v:
+      l.append(Gnuplot.Data(v, **{'with':"points ls %i"%(i+1), 'title':k}))
   g.plot(*l)
   while not os.path.exists(tmp_name):
      time.sleep(1)
--- types.py
+++ types.py
@@ -1,3 +1,4 @@
+import os
 import time
 
 import newegg
@@ -5,25 +6,27 @@ import graph
 import best
 
 types = [
-    ('sata', (2010150014, '359:15133', '359:7918')),
-    ('satas', (2010150380, '359:15133', '359:7918')),
-    ('usb', (2013240522,)),
-    ('ssd', (2010150636,)),
-    ('enclosure', (2010150414, '359:20386', '359:15635', '359:47642', '359:28161', '359:28610', '359:32416', '359:7839', '359:7851', '360:7801', '360:7802')),
+    ('sata', {None: (2010150014, '359:15133', '359:7918')}),
+    ('satas', {None: (2010150380, '359:15133', '359:7918')}),
+    ('usb', {None: (2013240522,)}),
+    ('ssd', {None: (2010150636,)}),
+    ('enclosure', {None: (2010150414, '359:20386', '359:15635', '359:47642', '359:28161', '359:28610', '359:32416', '359:7839', '359:7851', '360:7801', '360:7802')}),
 ]
 
-for type, call in types:
-    print type
-    
-    myitems = newegg.get(*call)
+result_dir = os.path.join(os.path.dirname(__file___), "result")
+
+for name, subtypes in types:
+    print name
     
     print 'Scraping'
     t = time.time()
-    myitems = list(myitems)
+    items = dict((subname, newegg.get(*call)) for subname, call in subtype.iteritems())
     
     print 'Graphing'
-    graph.graph(myitems, None, 'result/' + type + '.png')
+    graph.graph(items, os.path.join(result_dir, name + '.png'))
     
     print 'Itemizing'
-    b = best.best(myitems, 'result/' + type + '.txt')
-    open('record', 'a').write('%s %f %f\n' % (type, t, b))
+    b = best.best(items, os.path.join(result_dir, name + '.txt'))
+    
+    record = name, t, b
+    open(os.path.join(result_dir, 'record.txt'), 'a').write(repr(record) + "\n")
